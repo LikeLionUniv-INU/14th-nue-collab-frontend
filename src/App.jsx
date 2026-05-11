84f42d62651ffd36ffb9509e32aa2929bb49294d
cy123-coder <wolgugu@gmail.com>
2026-05-12 01:14:02 +0900

    Feat: 생년월일 페이지 구현 중 (#13)

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./styles/Layout";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+<<<<<<< HEAD
 import Intro1 from "./pages/Intro1";
 import Intro2 from "./pages/Intro2";
 import Intro3 from "./pages/Intro3";
 import Loading from "./pages/Loading";
+=======
+import Intro1 from "./pages/intro1";
+import Intro2 from "./pages/intro2";
+>>>>>>> 274526c207304eefaafcbd1c712d4f923e3d26c2
 import Birth from "./pages/Birth";
 import ResultPage from "./pages/ResultPage";
 
